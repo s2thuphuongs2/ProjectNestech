31642bfe287ef4ce7bf3862384a52b1bdb8b2fa0
PT <s2thuphuongs2@gmail.com>
2024-03-25 20:46:52 +0700

    init project

---
+++ src/main/java/com/example/projectnestech/ProjectNestechApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectnestech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNestechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNestechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectnestech/ProjectNestechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectnestech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNestechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
