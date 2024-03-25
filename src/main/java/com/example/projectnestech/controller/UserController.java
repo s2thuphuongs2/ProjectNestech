f6f84649092d80abe07621044c70622144d5e2c7
Juicyman2805 <votranminhman@gmail.com>
2024-03-25 21:17:05 +0700

    main

---
+++ src/main/java/com/example/projectnestech/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.projectnestech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/user")
+public class UserController {
+}
